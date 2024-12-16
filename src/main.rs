eb4d5b15d06f5e1c7fc883c335555112988339de
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-12-16 19:34:12 +0900

    feat: ターミナルサイズの取得

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use crossterm::terminal;
+
+fn main() -> Result<()> {
+    let term_width = terminal::size()?.0;
+    let term_height = terminal::size()?.1;
+    println!("{}x{}", term_width, term_height);
+    Ok(())
 }
