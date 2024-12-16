f915e51d2973233c8f06c5800b259a0dc24c6f26
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-12-16 19:56:28 +0900

    feat: 画像読み込み

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::io::Result;
 
 use crossterm::terminal;
+use image::GenericImageView;
 
 fn main() -> Result<()> {
     let term_width = terminal::size()?.0;
     let term_height = terminal::size()?.1;
     println!("{}x{}", term_width, term_height);
+
+    let image = match image::open("image.png") {
+        Ok(image) => image,
+        Err(e) => panic!("{}", e),
+    };
+    println!("dims: {:?}", image.dimensions());
     Ok(())
 }
