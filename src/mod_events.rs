c26f155771f06b60ca370d03e522eb24c04eeb78
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-12-17 11:10:54 +0900

    feat: 画像表示中に `o` 押下でファイル名の入力を行う

--- src/mod_events.rs
+++ src/mod_events.rs
@@ -1,6 +1,15 @@
-use std::io::Error;
+// use crate::mod_display::{DisplayInfo};
 
+use std::cmp::{max, min};
+use std::io::{stdout, Error, Write};
+
+use crossterm::cursor::MoveToColumn;
+
+use crossterm::event::KeyCode::{Backspace, Char, Delete, End, Enter, Esc, Home, Left, Right};
 use crossterm::event::{read, Event, KeyEvent, KeyEventKind, KeyModifiers};
+use crossterm::queue;
+use crossterm::style::Print;
+use crossterm::terminal::{Clear, ClearType};
 
 pub fn handle_events() -> Result<bool, Error> {
     match read() {
@@ -21,14 +30,86 @@ fn handle_key_events(key_event: KeyEvent) -> Result<bool, Error> {
     match key_event.modifiers {
         // simple key
         KeyModifiers::NONE => match key_event.code {
-            crossterm::event::KeyCode::Char('q') => Ok(false),
+            Char('q') => Ok(false),
+            Char('o') => {
+                let file_path = input_new_file_path()?;
+                println!("file_path: {}", file_path);
+                Ok(true)
+            }
             _ => Ok(true),
         },
         // ctrl pressed
         KeyModifiers::CONTROL => match key_event.code {
-            crossterm::event::KeyCode::Char('c') => Ok(false),
+            Char('c') => Ok(false),
             _ => Ok(true),
         },
         _ => Ok(true),
+    }
+}
+
+fn input_new_file_path() -> Result<String, Error> {
+    // show input message
+    let input_msg = "input new file path: ";
+    print!("{}", input_msg);
+    stdout().flush().unwrap();
+
+    // get input
+    let mut input = String::new();
+    let mut pos = 0;
+    loop {
+        match read() {
+            Ok(Event::Key(key_event)) if key_event.kind == KeyEventKind::Press => {
+                match key_event.code {
+                    Char(c) => {
+                        if c == 'c' && key_event.modifiers == KeyModifiers::CONTROL {
+                            return Err(Error::new(
+                                std::io::ErrorKind::Interrupted,
+                                "Input was canceled by user",
+                            ));
+                        }
+
+                        input.insert(pos, c);
+                        pos += 1;
+                    }
+                    Backspace => {
+                        if pos > 0 {
+                            input.remove(pos - 1);
+                            pos -= 1;
+                        }
+                    }
+                    Delete => {
+                        if pos < input.len() {
+                            input.remove(pos);
+                        }
+                    }
+                    Enter => {
+                        println!();
+                        return Ok(input);
+                    }
+
+                    Left => pos = max(0, pos - 1),
+                    Right => pos = min(input.len(), pos + 1),
+                    Home => pos = 0,
+                    End => pos = input.len(),
+
+                    Esc => {
+                        return Err(Error::new(
+                            std::io::ErrorKind::Interrupted,
+                            "Input was canceled by user",
+                        ));
+                    }
+                    _ => {}
+                }
+                queue!(stdout(), MoveToColumn(0)).unwrap();
+                queue!(stdout(), Print(input_msg), Print(&input)).unwrap();
+                queue!(stdout(), Clear(ClearType::UntilNewLine)).unwrap();
+                queue!(stdout(), MoveToColumn(input_msg.len() as u16 + pos as u16)).unwrap();
+                stdout().flush().unwrap();
+            }
+            Err(e) => {
+                return Err(e);
+            }
+            _ => {}
+        }
     }
 }
